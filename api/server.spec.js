b5da5a12d28d248f7bd13abd05a75ff4a7c90654
Becheru888 <becheru93@gmail.com>
2019-07-25 16:00:40 +0100

    post endpoint tested

--- api/server.spec.js
+++ api/server.spec.js
@@ -6,9 +6,21 @@ describe('server', () =>{
         return request(server)
         .get('/api/users')
         .expect(200)
-        .expect('Content-Type', /json/)  
+        .expect('Content-Type', /json/)
+        .then(res => {
+            expect(res.body).toBeInstanceOf(Array);
+        });
     });
 });
 
 
-
+describe('server', () =>{
+    it('ADD WORKS?', () =>{
+        return request(server)
+        .post('/api/users/add')
+        .expect('Content-Type', /json/)  
+        .then(req => {
+            expect(req.body).toBeInstanceOf(Object);
+        })
+    });
+});
