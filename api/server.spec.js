f53fc9caece0bc17a416c5a52ab43e8e3da2f81a
Becheru888 <becheru93@gmail.com>
2019-07-25 15:19:56 +0100

    Server jest configured

---
+++ api/server.spec.js
@@ -0,0 +1,11 @@
+const server = require('./server');
+const request = require('supertest')
+
+describe('server', () =>{
+    it('GET WORKS?', () =>{
+        return request(server)
+        .get('/api/users')
+        .expect(200)
+        .expect('Content-Type', /json/)  
+    });
+});
